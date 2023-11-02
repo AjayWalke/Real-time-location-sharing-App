d2404c15be50d737f54e0e13405bf0584f9da89f
AjayWalke <ajaywalke49@gmail.com>
2023-11-02 17:19:43 +0530

    socketUrl

--- client/src/services/soketURL.ts
+++ client/src/services/soketURL.ts
@@ -1,2 +1,2 @@
-export const Socket_URL = 'http://localhost:8080'
-// export const Socket_URL = ''
+// export const Socket_URL = 'http://localhost:8080'
+export const Socket_URL = 'https://real-time-location-sharing-app.onrender.com'
